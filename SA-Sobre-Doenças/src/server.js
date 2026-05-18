391aa8bfe628292b9587f71d7753b098110b7104
Fushikkj <xiguemartins@gmail.com>
2026-05-18 14:29:36 -0300

    atualização SA

--- SA-Sobre-Doenças/src/server.js
+++ SA-Sobre-Doenças/src/server.js
@@ -1,5 +1,5 @@
-const express = require("express");
-const app = express();
+import express from "express";
+export default router;
 
 app.use(express.json());
 
--- SA-Sobre-Doenças/src/service/doenca.service.js
+++ SA-Sobre-Doenças/src/service/doenca.service.js
@@ -1,13 +1,38 @@
 import fs from "fs/promises";
 
-const path = "./src/doencas.json";
+const path = "./src/service/doencas.json";
 
 async function lerDoencas() {
-  const data = await fs.readFile(path, "utf-8");
-  return JSON.parse(data);
+    const data = await fs.readFile(path, "utf-8");
+    return JSON.parse(data);
 }
 
 async function salvarDoencas(doencas) {
-  const data = JSON.stringify(doencas, null, 2);
-  await fs.writeFile(path, data);
+    await fs.writeFile(path, JSON.stringify(doencas, null, 2));
 }
+
+export default {
+    async getAll() {
+        return await lerDoencas();
+    },
+
+    async getById(id) {
+        const doencas = await lerDoencas();
+        return doencas.find(d => d.id == id);
+    },
+
+    async create(nome, fatal) {
+        const doencas = await lerDoencas();
+
+        const nova = {
+            id: doencas.length + 1,
+            nome,
+            fatal
+        };
+
+        doencas.push(nova);
+        await salvarDoencas(doencas);
+
+        return nova;
+    }
+};
